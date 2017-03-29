c4b82d5c67bd8b14089698aacc79a0264c653fa9
will <will@localhost.localdomain>
2017-03-29 19:55:43 -0300

    documents migration

---
+++ database/migrations/2017_03_29_192809_create_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->integer('documentId')->unsigned();
+            $table->string('documentName');
+            $table->integer('flagCompany');
+            $table->primary(['documentId'], 'pk_d_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
