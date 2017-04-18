7be3dae1e09c999f73baf3d1c28c8e6871e6f3c4
jurandicostaoliveira <jurandico@gmail.com>
2017-04-18 19:47:59 -0300

    Dev user of the provider

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -12,39 +12,60 @@ class Provider extends Model
 
     protected $fillable = [
         'name',
+        'fantasyName',
+        'activityBranch',
         'cnpj',
         'stateInscription',
         'municipalInscription',
         'mainCnae',
-        'activityBranch',
-        'cep',
-        'number',
-        'addressComplement',
         'phone',
         'fax',
-        'email'
+        'cep',
+        'street',
+        'number',
+        'district',
+        'city',
+        'state',
+        'responsibleName',
+        'cellPhone',
+        'email',
+        'createdAt',
+        'updatedAt'
     ];
 
     /**
      * Rules of the validation
      *
      * @param null $id
+     * @param null $userId
      * @return array
      */
-    public function validateRules($id = null)
+    public function validateRules($id = null, $userId = null)
     {
         return [
             'name' => 'required',
+            'fantasyName' => 'required',
+            'activityBranch' => 'required',
             'cnpj' => 'required|unique_multiple:companies,cnpj,name,id=' . $id,
             'stateInscription' => 'required',
             'municipalInscription' => 'required',
             'mainCnae' => 'required',
-            'activityBranch' => 'required',
-            'cep' => 'required',
-            'number' => 'required',
-            'addressComplement' => 'required',
             'phone' => 'required',
-            'email' => 'required'
+            'fax' => 'required',
+            'cep' => 'required',
+            'street' => 'required',
+            'number' => 'required',
+            'district' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'responsibleName' => 'required',
+
+            'cpf' => 'required|unique_multiple:users,cpf,id=' . $userId,
+            'jobRole' => 'required',
+            'department' => 'required',
+            'email' => 'email|unique_multiple:users,email,id=' . $userId,
+            'password' => 'required|min:6|max:14'
+
         ];
     }
 
@@ -57,17 +78,32 @@ class Provider extends Model
     {
         return [
             'name.required' => 'O campo nome é obrigatório.',
+            'fantasyName' => 'O campo nome fantasia é obrigatório.',
+            'activityBranch.required' => 'O campo ramo de atividade é obrigatório.',
             'cnpj.required' => 'O campo cnpj é obrigatório.',
-            'cnpj.unique_multiple' => 'O nome e o cnpj do prestador de serviços já existe.',
+            'cnpj.unique_multiple' => 'O nome e o cnpj da empresa já existe.',
             'stateInscription.required' => 'O campo inscrição estadual é obrigatório.',
             'municipalInscription.required' => 'O campo inscrição minicipal é obrigatório.',
             'mainCnae.required' => 'O campo cnae principal é obrigatório.',
-            'activityBranch.required' => 'O campo ramo de atividade é obrigatório.',
-            'cep.required' => 'O campo cep é obrigatório.',
-            'number.required' => 'O campo numero é obrigatório.',
-            'addressComplement.required' => 'O campo complemento é obrigatório.',
             'phone.required' => 'O campo telefone é obrigatório.',
-            'email.required' => 'O campo e-mail é obrigatório.'
+            'fax.required' => 'O campo fax é obrigatório.',
+            'cep.required' => 'O campo cep é obrigatório.',
+            'street.required' => 'O campo logradouro é obrigatório.',
+            'number.required' => 'O campo número é obrigatório.',
+            'district.required' => 'O campo bairro é obrigatório.',
+            'city.required' => 'O campo cidade é obrigatório.',
+            'state.required' => 'O campo UF é obrigatório.',
+            'responsibleName.required' => 'O campo nome responsável é obrigatório.',
+
+            'cpf.required' => 'O campo cpf é obrigatório.',
+            'cpf.unique_multiple' => 'O cpf já existe.',
+            'jobRole.required' => 'O campo cargo é obrigatório.',
+            'department.required' => 'O campo setor é obrigatório.',
+            'email.email' => 'O campo e-mail deve ser um endereço de e-mail válido.',
+            'email.unique_multiple' => 'O e-mail já existe.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.min' => 'O campo senha deve ter mínimo de 6 e no máximo 14 caracteres.',
+            'password.max' => 'O campo senha deve ter mínimo de 6 e no máximo 14 caracteres.'
         ];
     }
 
--- resources/views/panel/provider/form.blade.php
+++ resources/views/panel/provider/form.blade.php
@@ -3,105 +3,186 @@
 @section('title', 'Gest&atilde;o de prestador de servi&ccedil;os')
 
 @section('content')
-    <!-- page content -->
-    <div class="right_col" role="main">
+        <!-- page content -->
+<div class="right_col" role="main">
 
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <!-- menu breadcrumb -->
-                        @include('includes.breadcrumb')
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <!-- menu breadcrumb -->
+                    @include('includes.breadcrumb')
+                </div>
+                <div class="x_content">
+
+                    <div style="padding: 10px 0px 20px 10px">
+                        <strong>Atenção : </strong>todos os campos com o s&iacute;mbolo * s&atilde;o obrigat&oacute;rios.
                     </div>
-                    <div class="x_content">
 
-                        <div style="padding: 10px 0px 20px 10px">
-                            <strong>Atenção : </strong>todos os campos com o s&iacute;mbolo * s&atilde;o obrigat&oacute;rios.
+                    @include('includes.form-validate')
+
+                    <form id="provider-form" method="post" action="{{ route($route, $parameters) }}">
+
+                        {{ method_field($method) }}
+                        {{ csrf_field() }}
+
+
+                        <div class="form-group col-xs-4">
+                            <label for="name">Razão Social* :</label>
+                            <input type="text" id="name" name="name" value="{{ $provider->name or old('name') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="fantasyName">Nome Fantasia* :</label>
+                            <input type="text" id="fantasyName" name="fantasyName"
+                                   value="{{ $provider->fantasyName or old('fantasyName') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="activityBranch">Ramo de Atividade* :</label>
+                            <input type="text" id="activityBranch" name="activityBranch"
+                                   value="{{ $provider->activityBranch or old('activityBranch') }}"
+                                   class="form-control" required>
                         </div>
 
-                        @include('includes.form-validate')
+                        <div class="form-group col-xs-4">
+                            @if(isset($provider->cnpjHidden))
+                                <label>CNPJ* :</label>
+                                <input type="text" value="{{ $provider->cnpj }}" class="form-control" disabled>
+                                <input type="hidden" name="cnpj" value="{{ $provider->cnpj }}">
+                            @else
+                                <label for="cpnj">CNPJ* : </label>
+                                <input type="text" id="cnpj" name="cnpj"
+                                       value="{{ $provider->cnpj or old('cnpj') }}" class="form-control" required>
+                            @endif
+                        </div>
 
-                        <form id="provider-form" method="post" action="{{ route($route, $parameters) }}">
+                        <div class="form-group col-xs-4">
+                            <label for="stateInscription">Inscrição Estadual* :</label>
+                            <input type="text" id="stateInscription" name="stateInscription"
+                                   value="{{ $provider->stateInscription or old('stateInscription') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="municipalInscription">Inscrição Municipal* :</label>
+                            <input type="text" id="municipalInscription" name="municipalInscription"
+                                   value="{{ $provider->municipalInscription or old('municipalInscription') }}"
+                                   class="form-control" required>
+                        </div>
 
-                            {{ method_field($method) }}
-                            {{ csrf_field() }}
 
-                            <div class="form-group col-xs-4">
-                                <label for="name">Nome* :</label>
-                                <input type="text" id="name" name="name" value="{{ $provider->name or old('name') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                @if(isset($provider->cnpjHidden))
-                                    <label>CNPJ* :</label>
-                                    {{ $provider->cnpj }}
-                                    <input type="hidden" name="cnpj" value="{{ $provider->cnpj }}">
-                                @else
-                                    <label for="cpnj">CNPJ* : </label>
-                                    <input type="text" id="cnpj" name="cnpj"
-                                           value="{{ $provider->cnpj or old('cnpj') }}" class="form-control" required>
-                                @endif
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="stateInscription">Inscrição Estadual* :</label>
-                                <input type="text" id="stateInscription" name="stateInscription" value="{{ $provider->stateInscription or old('stateInscription') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="municipalInscription">Inscrição Municipal* :</label>
-                                <input type="text" id="municipalInscription" name="municipalInscription" value="{{ $provider->municipalInscription or old('municipalInscription') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="mainCnae">CNAE Principal* :</label>
-                                <input type="text" id="mainCnae" name="mainCnae" value="{{ $provider->mainCnae or old('mainCnae') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="activityBranch">Ramo de Atividade* :</label>
-                                <input type="text" id="activityBranch" name="activityBranch" value="{{ $provider->activityBranch or old('activityBranch') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="cep">CEP* :</label>
-                                <input type="text" id="cep" name="cep" value="{{ $provider->cep or old('cep') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="number">Número* :</label>
-                                <input type="text" id="number" name="number" value="{{ $provider->number or old('number') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="addressComplement">Complemento* :</label>
-                                <input type="text" id="addressComplement" name="addressComplement" value="{{ $provider->addressComplement or old('addressComplement') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="phone">Telefone* :</label>
-                                <input type="phone" id="phone" name="phone" value="{{ $provider->phone or old('phone') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="fax">Fax :</label>
-                                <input type="fax" id="fax" name="fax" value="{{ $provider->fax or old('fax') }}"
-                                       class="form-control">
-                            </div>
-                            <div class="form-group col-xs-4">
-                                <label for="email">E-mail* :</label>
-                                <input type="email" id="email" name="email" value="{{ $provider->email or old('email') }}"
-                                       class="form-control" required>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="control-group" style="margin: 30px 0px 0px 12px;">
-                                <button type="submit" class="btn btn-success">Salvar</button>
-                            </div>
-                        </form>
+                        <div class="form-group col-xs-4">
+                            <label for="mainCnae">CNAE Principal* :</label>
+                            <input type="text" id="mainCnae" name="mainCnae"
+                                   value="{{ $provider->mainCnae or old('mainCnae') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="phone">Telefone* :</label>
+                            <input type="phone" id="phone" name="phone"
+                                   value="{{ $provider->phone or old('phone') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="fax">Fax* :</label>
+                            <input type="fax" id="fax" name="fax" value="{{ $provider->fax or old('fax') }}"
+                                   class="form-control">
+                        </div>
 
-                    </div>
+
+                        <div class="form-group col-xs-4">
+                            <label for="cep">CEP* :</label>
+                            <input type="text" id="cep" name="cep" value="{{ $provider->cep or old('cep') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="street">Logradouro* :</label>
+                            <input type="text" id="street" name="street"
+                                   value="{{ $provider->street or old('street') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="number">Número* :</label>
+                            <input type="text" id="number" name="number"
+                                   value="{{ $provider->number or old('number') }}"
+                                   class="form-control" required>
+                        </div>
+
+
+                        <div class="form-group col-xs-4">
+                            <label for="district">Bairro* :</label>
+                            <input type="text" id="district" name="district"
+                                   value="{{ $provider->district or old('district') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="city">Cidade* :</label>
+                            <input type="text" id="city" name="city" value="{{ $provider->city or old('city') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="state">UF* :</label>
+                            <select type="text" id="state" name="state" class="form-control">
+                                @foreach($states as $key => $value)
+                                    <option value="{{ $key }}"
+                                            @if($provider->state === $key) selected @endif>{{ $value }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group col-xs-12">
+                            <h2>Dados do respons&aacute;vel :</h2>
+                        </div>
+
+                        <div class="form-group col-xs-4">
+                            <label for="responsibleName">Nome * :</label>
+                            <input type="responsibleName" id="responsibleName" name="responsibleName"
+                                   value="{{ $provider->responsibleName or old('responsibleName') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="cpf">CPF * :</label>
+                            <input type="cpf" id="cpf" name="cpf"
+                                   value=""
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="jobRole">Cargo * :</label>
+                            <input type="jobRole" id="jobRole" name="jobRole"
+                                   value=""
+                                   class="form-control" required>
+                        </div>
+
+                        <div class="form-group col-xs-4">
+                            <label for="department">Setor * :</label>
+                            <input type="department" id="department" name="department"
+                                   value=""
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="email">E-mail * :</label>
+                            <input type="email" id="email" name="email"
+                                   value="{{ $provider->email or old('email') }}"
+                                   class="form-control" required>
+                        </div>
+                        <div class="form-group col-xs-4">
+                            <label for="password">Senha * :</label>
+                            <input type="password" id="password" name="password"
+                                   value="{{ $provider->email or old('email') }}"
+                                   class="form-control" required>
+                        </div>
+
+                        <div class="clearfix"></div>
+                        <div class="control-group" style="margin: 30px 0px 0px 12px;">
+                            <button type="submit" class="btn btn-success">Salvar</button>
+                        </div>
+
+                    </form>
+
                 </div>
             </div>
         </div>
     </div>
-    <!-- /page content -->
+</div>
+<!-- /page content -->
 @endsection
