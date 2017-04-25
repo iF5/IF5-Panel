aebad2f981af6ccb6afa6982f13ef42b9cd04355
Jurandi <jurandico@gmail.com>
2017-04-25 09:06:24 -0300

    Dev employee show

--- resources/views/panel/provider/show.blade.php
+++ resources/views/panel/provider/show.blade.php
@@ -16,9 +16,41 @@
 
                 <ul class="list-group">
                     <li class="list-group-item"><strong>Nome : </strong> {{ $provider->name }}</li>
+                    <li class="list-group-item"><strong>Nome Fantasia : </strong> {{ $provider->fantasyName }}</li>
+                    <li class="list-group-item"><strong>Ramo de Atividade : </strong> {{ $provider->activityBranch }}
+                    </li>
+
+
                     <li class="list-group-item"><strong>CNPJ : </strong> {{ $provider->cnpj }}</li>
+                    <li class="list-group-item"><strong>Inscrição Estadual : </strong> {{ $provider->stateInscription }}
+                    </li>
+                    <li class="list-group-item"><strong>Inscrição Municipal
+                            : </strong> {{ $provider->municipalInscription }}
+                    </li>
+
+                    <li class="list-group-item"><strong>CNAE Principal : </strong> {{ $provider->mainCnae }}</li>
+                    <li class="list-group-item"><strong>Telefone : </strong> {{ $provider->phone }}</li>
+                    <li class="list-group-item"><strong>Fax : </strong> {{ $provider->fax }}</li>
+
+                    <li class="list-group-item"><strong>CEP : </strong> {{ $provider->cep }}</li>
+                    <li class="list-group-item"><strong>Logradouro : </strong> {{ $provider->street }}</li>
+                    <li class="list-group-item"><strong>Número : </strong> {{ $provider->number }}</li>
+
+                    <li class="list-group-item"><strong>Bairro : </strong> {{ $provider->district }}</li>
+                    <li class="list-group-item"><strong>Cidade : </strong> {{ $provider->city }}</li>
+                    <li class="list-group-item"><strong>UF : </strong> {{ $provider->state }}</li>
+
+                    <li class="list-group-item"><strong>Nome responsável : </strong> {{ $provider->responsibleName }}
+                    </li>
+                    <li class="list-group-item"><strong>Celular: </strong> {{ $provider->cellPhone }}</li>
+                    <li class="list-group-item"><strong>E-mail: </strong> {{ $provider->email }}</li>
+                    
                     <li class="list-group-item"><strong>Status
                             : </strong> {{ ($provider->status)? 'Ok' : 'Aguardando aprovação' }}</li>
+                    <li class="list-group-item"><strong>Cadastrado em
+                            : </strong> {{ \Carbon\Carbon::parse($provider->createdAt)->format('d/m/Y H:i:s') }}</li>
+                    <li class="list-group-item"><strong>&Uacute;ltima atualiza&ccedil;&atilde;o
+                            : </strong> {{ \Carbon\Carbon::parse($provider->updatedAt)->format('d/m/Y H:i:s') }}</li>
                     <li class="list-group-item">
                         @if(!$provider->status && \Auth::user()->role === 'admin')
                             <a href="#"
@@ -40,9 +72,9 @@
                         @endif
 
                         @can('onlyAdmin')
-                        <a href="{{ route('provider.edit', $provider->id) }}"
-                           class="btn btn-success btn-md"><span
-                                    class="glyphicon glyphicon-pencil"></span></a>
+                            <a href="{{ route('provider.edit', $provider->id) }}"
+                               class="btn btn-success btn-md"><span
+                                        class="glyphicon glyphicon-pencil"></span></a>
                         @endcan
 
                         <a href="#"
