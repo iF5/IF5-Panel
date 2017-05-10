da31b1823693ac6c341e0a5c15fe0f525b81fd04
Jurandi <jurandico@gmail.com>
2017-05-10 08:05:02 -0300

    Create Report Controller

--- app/Http/Controllers/Panel/ReportController.php
+++ app/Http/Controllers/Panel/ReportController.php
@@ -62,7 +62,7 @@ class ReportController
     {
         $referenceDate = \Request::input('reference-date');
         $reports = ($referenceDate) ?
-            $this->reportRepository->findLike($this->getCompanyId(), $referenceDate) :
+            $this->reportRepository->findByReferenceDate($this->getCompanyId(), $referenceDate) :
             $this->reportRepository->findOrderBy($this->getCompanyId());
 
         return view('panel.report.list', [
@@ -71,6 +71,7 @@ class ReportController
             'breadcrumbs' => $this->getBreadcrumb()
         ]);
     }
+
 
     public function upload(Request $request)
     {
@@ -85,6 +86,114 @@ class ReportController
             'message' => 'Os Arquivos foram enviados com sucesso!'
         ]);
 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('panel.report.form', [
+            'report' => $this->reportRepository,
+            'method' => 'POST',
+            'route' => 'report.store',
+            'parameters' => [],
+            'breadcrumbs' => $this->getBreadcrumb('Cadastrar')
+        ]);
+    }
+
+    protected function formRequest($data, $action = null)
+    {
+        if ($action === 'store') {
+            $data['createdAt'] = (new \DateTime())->format('Y-m-d H:i:s');
+            $data['companyId'] = $this->getCompanyId();
+        }
+        return $data;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate(
+            $request, $this->reportRepository->validateRules(), $this->reportRepository->validateMessages()
+        );
+
+        $data = $this->formRequest($request->all(), 'store');
+        $this->reportRepository->create($data);
+
+        return redirect()->route('report.create')->with([
+            'success' => true,
+            'message' => 'Relat&oacute;rio cadastrado com sucesso!'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('panel.report.form', [
+            'report' => $this->reportRepository->findOrFail($id),
+            'route' => 'report.update',
+            'method' => 'PUT',
+            'parameters' => [$id],
+            'breadcrumbs' => $this->getBreadcrumb('Editar')
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate(
+            $request, $this->reportRepository->validateRules(), $this->reportRepository->validateMessages()
+        );
+
+        $data = $this->formRequest($request->all());
+        $this->reportRepository->findOrFail($id)->update($data);
+
+        return redirect()->route('report.edit', $id)->with([
+            'success' => true,
+            'message' => 'Relat&oacute;rio atualizado com sucesso!'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->reportRepository->destroy($id);
+        return redirect()->route('report.index');
     }
 
     /**
