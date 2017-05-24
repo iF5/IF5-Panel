fad024d2860e14c4ee11469ca96df9be169eb765
jurandicostaoliveira <jurandico@gmail.com>
2017-05-24 20:34:47 -0300

    Dev dashboard part 3

--- app/Http/Controllers/Panel/DashboardController.php
+++ app/Http/Controllers/Panel/DashboardController.php
@@ -11,11 +11,68 @@ class DashboardController extends Controller
     {
        //$this->authorize('isProvider');
 
-        $documentsTitle = \DB::table('documents')->get();
+        $documents = \DB::table('documents')->get();
+        $total = count($documents);
+
+        $join = \DB::select("SELECT
+        a.documentId,
+        a.documentQuantity,
+        b.providerId,
+        b.providerName,
+        b.employeeQuantity
+        FROM (
+        (
+            SELECT
+            ehd.documentId,
+            count(ehd.documentId) AS documentQuantity,
+            e.providerId
+            FROM documents AS d
+            LEFT JOIN employees_has_documents AS ehd
+            ON ehd.documentId = d.id
+            INNER JOIN employees AS e
+            ON e.id = ehd.employeeId
+            GROUP BY d.id, e.providerId
+        ) AS a,
+        (
+            SELECT
+            p.id AS providerId,
+            p.name AS providerName,
+            COUNT(e.id) AS employeeQuantity
+            FROM providers AS p
+            INNER JOIN employees AS e
+            ON e.providerId = p.id
+            GROUP BY p.id
+            ) AS b
+        ) WHERE a.providerId = b.providerId;");
+
+        $arr = [];
+        $values = [];
+        foreach($join as $j){
+            for($i = 1; $i <= $total; $i++){
+                if($j->documentId === $i){
+                    $values = [
+                        'providerName' => $j->providerName,
+                        'employeeQuantity' => $j->employeeQuantity,
+                        'documentQuantity' => $j->documentQuantity
+                    ];
+                }else{
+                    $values = [
+                        'providerName' => $j->providerName,
+                        'employeeQuantity' => $j->employeeQuantity,
+                        'documentQuantity' => 0
+                    ];
+                }
+            }
+            $arr[$j->providerId][] = $values;
+        }
+
+        dd($arr);
+
+        dd('END');
 
         return view('panel.dashboard.index', [
-            'documentsTitle' => $documentsTitle,
-            'total' => count($documentsTitle)
+            'documentsTitle' => $documents,
+            'total' => $total
         ]);
         /**
         SELECT
@@ -49,28 +106,6 @@ class DashboardController extends Controller
         ) AS b
         ) WHERE a.providerId = b.providerId;
 
-        SELECT
-        p.id,
-        p.name,
-        COUNT(e.id) AS employeeQuantity
-        FROM providers AS p
-        INNER JOIN employees AS e
-        ON e.providerId = p.id
-        WHERE p.name LIKE '%tok%'
-        GROUP BY p.id;
-
-
-        SELECT
-        d.id,
-        d.name,
-        count(ehd.documentId) AS documentQuantity
-        FROM documents AS d
-        LEFT JOIN employees_has_documents AS ehd
-        ON ehd.documentId = d.id
-        LEFT JOIN employees AS e
-        ON e.id = ehd.employeeId
-        WHERE e.providerId = 1
-        GROUP BY d.id;
          */
 
     }
