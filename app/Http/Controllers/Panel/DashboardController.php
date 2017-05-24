1a07c37c36fb5b5bb7279563ebb7c3c266614848
Jurandi <jurandico@gmail.com>
2017-05-24 09:03:20 -0300

    Dev dashboard part 2

--- app/Http/Controllers/Panel/DashboardController.php
+++ app/Http/Controllers/Panel/DashboardController.php
@@ -19,6 +19,37 @@ class DashboardController extends Controller
         ]);
         /**
         SELECT
+        a.documentId,
+        a.documentQuantity,
+        b.providerId,
+        b.providerName,
+        b.employeeQuantity
+        FROM (
+        (
+        SELECT
+        ehd.documentId,
+        count(ehd.documentId) AS documentQuantity,
+        e.providerId
+        FROM documents AS d
+        LEFT JOIN employees_has_documents AS ehd
+        ON ehd.documentId = d.id
+        INNER JOIN employees AS e
+        ON e.id = ehd.employeeId
+        GROUP BY d.id, e.providerId
+        ) AS a,
+        (
+        SELECT
+        p.id AS providerId,
+        p.name AS providerName,
+        COUNT(e.id) AS employeeQuantity
+        FROM providers AS p
+        INNER JOIN employees AS e
+        ON e.providerId = p.id
+        GROUP BY p.id
+        ) AS b
+        ) WHERE a.providerId = b.providerId;
+
+        SELECT
         p.id,
         p.name,
         COUNT(e.id) AS employeeQuantity
