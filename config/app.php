538ee36a1b04328dabf35caf7cdb8e2ed938d06e
will <will@localhost.localdomain>
2017-07-21 15:01:38 -0300

    SSL

--- config/app.php
+++ config/app.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @return string
+ */
+function getUrl()
+{
+    $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+    return $protocol . $_SERVER['HTTP_HOST'];
+}
+
 return [
 
     /*
@@ -51,7 +60,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', getUrl()),
 
     /*
     |--------------------------------------------------------------------------
