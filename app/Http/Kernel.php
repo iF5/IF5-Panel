68e4c0a4c7e3f1f3ec9ac7c6863275402abc74d8
will <will@localhost.localdomain>
2017-07-21 15:51:25 -0300

    SSL

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,10 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        /**
+         * To (http or https)
+         */
+        \App\Http\Middleware\HttpsProtocol::class
     ];
 
     /**
